0bb38754ef39efcaf94a114b0497da9279e35ce9
LuoTianouo <11837706+luotianouo@user.noreply.gitee.com>
2022-12-06 08:47:10 +0000

    update opencv_api/main_api.cpp.
    Signed-off-by: LuoTianouo <11837706+luotianouo@user.noreply.gitee.com>

--- opencv_api/main_api.cpp
+++ opencv_api/main_api.cpp
@@ -1,7 +1,8 @@
 #include <opencv.hpp>
 #include <iostream>
 #include <fstream>
-#include "QRcode.h"
+#include<Windows.h>
+#include "cv_user.h"
 using namespace cv;
 using namespace std;
 int main()
@@ -11,9 +12,18 @@ int main()
 		return -1;
 	camera.set(CAP_PROP_FRAME_HEIGHT, 720);
 	camera.set(CAP_PROP_FRAME_WIDTH, 1280);
+
+	//扫码演示
+	/*
 	string turns_info;
 	QRcode(&camera, &turns_info);
 	printf_s("\n%s\n", turns_info.c_str());
+	*/
+
+	//轮廓识别演示
+	vector<Point> figure_info;//中心点信息
+	vector<RotatedRect> rects;//轮廓矩阵信息
+	ColorFinding(&camera, &figure_info, &rects, 0);
 	waitKey(0);
 	return 0;
 }
