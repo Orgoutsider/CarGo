be4bd05668811cd0df099f3618c0e5dc1b9fa412
LuoTianouo <11837706+luotianouo@user.noreply.gitee.com>
2023-04-27 17:25:05 +0000

    update src/motion_controller/src/motion_controller/motion_controller.cpp.
    Signed-off-by: LuoTianouo <11837706+luotianouo@user.noreply.gitee.com>

--- src/motion_controller/src/motion_controller/motion_controller.cpp
+++ src/motion_controller/src/motion_controller/motion_controller.cpp
@@ -50,6 +50,7 @@ namespace motion_controller
         Mat srcF;
         resize(cv_image->image, srcF, Size(width_, height_));
         srcF = srcF(Range(r_start_, r_end_), Range(c_start_, c_end_));
+#if(0)//原先代码
         // 色彩分离查找车道线
         cvtColor(srcF, srcF, COLOR_BGR2HSV);
         inRange(srcF, black_low_, black_up_, srcF);
@@ -63,6 +64,76 @@ namespace motion_controller
             waitKey(1);
         }
         Canny(srcF, srcF, 50, 100, 3);
+#else//分水岭代码
+        cvtColor(srcImg, srcF_FenGe, COLOR_BGR2GRAY);
+        GaussianBlur(srcF_FenGe, srcF_FenGe, Size(3, 3), 0, 0);
+        Canny(srcF_FenGe, srcF_FenGe, 50, 100, 3);
+
+        vector<vector<Point>> contours;
+        vector<Vec4i> hierarchy;
+        findContours(srcF_FenGe, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point());
+        Mat imageContours = Mat::zeros(srcImg.size(), CV_8UC1);  //轮廓
+        Mat marks(srcImg.size(), CV_32S);
+        marks = Scalar::all(0);
+        int index = 0;
+        int compCount = 0;
+        //这个循环在使用时注意一下，可能会出问题
+        for (; index >= 0; index = hierarchy[index][0], compCount++)
+        {
+            drawContours(marks, contours, index, Scalar::all(compCount + 1), 1, 8, hierarchy);
+            drawContours(imageContours, contours, index, Scalar(255), 1, 8, hierarchy);
+        }
+        //中间图像，调试用
+        Mat marksShows;
+        convertScaleAbs(marks, marksShows);
+        imshow("marksShow", marksShows);
+        imshow("轮廓", imageContours);
+
+        watershed(srcImg, marks);//分水岭检测
+
+        //中间图像，调试用
+        Mat afterWatershed;
+        convertScaleAbs(marks, afterWatershed);
+        imshow("After Watershed", afterWatershed);
+
+        //对每一个区域进行颜色填充，调试用
+        Mat PerspectiveImage = Mat::zeros(srcImg.size(), CV_8UC3);
+        for (int i = 0; i < marks.rows; i++)
+        {
+            for (int j = 0; j < marks.cols; j++)
+            {
+                int index = marks.at<int>(i, j);
+                if (marks.at<int>(i, j) == -1)
+                {
+                    PerspectiveImage.at<Vec3b>(i, j) = Vec3b(255, 255, 255);
+                }
+                else
+                {
+                    PerspectiveImage.at<Vec3b>(i, j) = RandomColor(index);
+                }
+            }
+        }
+        imshow("After ColorFill", PerspectiveImage);
+
+        //分割并填充颜色的结果跟原始图像融合，调试用
+        Mat wshed;
+        addWeighted(srcImg, 0.4, PerspectiveImage, 0.6, 0, wshed);
+        imshow("AddWeighted Image", wshed);
+
+        //然后在这里接入轮廓的一些筛选和排除，然后进行直线识别
+        /*里面有用到一个随机颜色生成函数：
+        Vec3b RandomColor(int value)//随机颜色绘制函数。调试用
+        {
+            value = value % 255;
+            RNG rng;
+            int aa = rng.uniform(0, value);
+            int bb = rng.uniform(0, value);
+            int cc = rng.uniform(0, value);
+            return Vec3b(aa, bb, cc);
+        }
+        */
+
+#endif
         std::vector<Vec2f> lines;
         HoughLines(srcF, lines, 1, CV_PI / 180, threshold_, 0, 0); // 根据实际调整，夜晚40，下午50
         int tot = 0, y_sum = 0;
