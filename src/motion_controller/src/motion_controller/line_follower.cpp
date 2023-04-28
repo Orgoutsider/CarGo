560452a701e17c8474efa6bbc75eba01e23be874
nano <nano@nano.com>
2023-04-28 14:10:31 +0800

    nano watershed

--- src/motion_controller/src/motion_controller/line_follower.cpp
+++ src/motion_controller/src/motion_controller/line_follower.cpp
@@ -3,8 +3,8 @@
 namespace motion_controller
 {
     LineFollower::LineFollower(ros::NodeHandle &nh, ros::NodeHandle &pnh)
-        : r_start_(110), r_end_(150),
-          c_start_(10), c_end_(width_ - c_start_), threshold_(50),
+        : r_start_(110), r_end_(160),
+          c_start_(5), c_end_(width_ - c_start_), threshold_(50),
           judge_line_(10), linear_velocity_(0.2),
           rho_thr_(5), theta_thr_(3),
           kp_(0.0015), kd_(0.001),
@@ -72,7 +72,7 @@ namespace motion_controller
         Mat srcF;
         resize(cv_image->image, srcF, Size(width_, height_));
         srcF = srcF(Range(r_start_, r_end_), Range(c_start_, c_end_));
-        srcF.convertTo(srcF, CV_8UC3);
+        // srcF.convertTo(srcF, CV_8UC3);
         // // 原先代码
         // // 色彩分离查找车道线
         // cvtColor(srcF, srcF, COLOR_BGR2HSV);
@@ -138,147 +138,149 @@ namespace motion_controller
         }
 
         // 对每一个区域进行颜色填充，调试用
-        Mat PerspectiveImage = Mat::zeros(srcF.size(), CV_8UC3);
-        for (int i = 0; i < marks.rows; i++)
-        {
-            for (int j = 0; j < marks.cols; j++)
-            {
-                int index = marks.at<int>(i, j);
-                if (marks.at<int>(i, j) == -1)
-                {
-                    PerspectiveImage.at<Vec3b>(i, j) = Vec3b(255, 255, 255);
-                }
-                else
-                {
-                    // 随机颜色生成
-                    int value = index % 255;
-                    RNG rng;
-                    int aa = rng.uniform(0, value);
-                    int bb = rng.uniform(0, value);
-                    int cc = rng.uniform(0, value);
-                    PerspectiveImage.at<Vec3b>(i, j) = Vec3b(aa, bb, cc);
-                }
-            }
-        }
-        imshow("After ColorFill", PerspectiveImage);
-
-        // 分割并填充颜色的结果跟原始图像融合，调试用
         if (param_modification_)
         {
+
+            Mat PerspectiveImage = Mat::zeros(srcF.size(), CV_8UC3);
+            for (int i = 0; i < marks.rows; i++)
+            {
+                for (int j = 0; j < marks.cols; j++)
+                {
+                    int index = marks.at<int>(i, j);
+                    if (marks.at<int>(i, j) == -1)
+                    {
+                        PerspectiveImage.at<Vec3b>(i, j) = Vec3b(255, 255, 255);
+                    }
+                    else
+                    {
+                        // 随机颜色生成
+                        int value = index % 255;
+                        RNG rng;
+                        int aa = rng.uniform(0, value);
+                        int bb = rng.uniform(0, value);
+                        int cc = rng.uniform(0, value);
+                        PerspectiveImage.at<Vec3b>(i, j) = Vec3b(aa, bb, cc);
+                    }
+                }
+            }
+            imshow("After ColorFill", PerspectiveImage);
+            // 分割并填充颜色的结果跟原始图像融合，调试用
             Mat wshed;
             addWeighted(srcF, 0.4, PerspectiveImage, 0.6, 0, wshed);
             imshow("AddWeighted Image", wshed);
+            imshow("srcF", srcF);
+            waitKey(1);
         }
 
-        // 然后在这里接入轮廓的一些筛选和排除，然后进行直线识别
-        std::vector<Vec2f> lines;
-        HoughLines(srcF, lines, 1, CV_PI / 180, threshold_, 0, 0); // 根据实际调整，夜晚40，下午50
-        int minx, maxx, nowx;
-        static int last_err = 0, flag = 0;
-        geometry_msgs::Twist twist;
+        // // 然后在这里接入轮廓的一些筛选和排除，然后进行直线识别
+        // std::vector<Vec2f> lines;
+        // HoughLines(srcF, lines, 1, CV_PI / 180, threshold_, 0, 0); // 根据实际调整，夜晚40，下午50
+        // int minx, maxx, nowx;
+        // static int last_err = 0, flag = 0;
+        // geometry_msgs::Twist twist;
 
-        // 直线筛选参数
-        int num[2] = {0}; // 筛选后的直线参数
+        // // 直线筛选参数
+        // int num[2] = {0}; // 筛选后的直线参数
 
-        // 直线拟合参数 左0右1
-        const int left = 0;
-        const int right = 1;
-        const int MAXN = lines.size();
-        double rho_aver[2] = {0, 0};
-        double theta_aver[2] = {0, 0};
-        Vec2f lines_left_right[2][MAXN];
-        double x[2] = {0};
+        // // 直线拟合参数 左0右1
+        // const int left = 0;
+        // const int right = 1;
+        // const int MAXN = lines.size();
+        // double rho_aver[2] = {0, 0};
+        // double theta_aver[2] = {0, 0};
+        // Vec2f lines_left_right[2][MAXN];
+        // double x[2] = {0};
 
-        if (lines.size() > 1)
-        {
-            // 遍历直线求平均，分成左右两边
-            for (Vec2f &line : lines)
-            {
-                double rho = line[0], theta = line[1];
-                // if(abs(rho-rho_NUM)>rho_Derta && abs(theta-theta_NUM)>theta_Derta || abs(theta) < 3)//最后这个是滤去一些theta错误的直线，可以不加
-                //     continue;
-                if (theta > 0)
-                {
-                    rho_aver[left] += rho;
-                    theta_aver[left] += theta;
-                    lines_left_right[left][num[left]] = line;
-                    num[left]++;
-                }
-                else if (theta < 0)
-                {
-                    rho_aver[right] += rho;
-                    theta_aver[right] += theta;
-                    lines_left_right[right][num[right]] = line;
-                    num[right]++;
-                }
-                if (param_modification_)
-                {
-                    double a = cos(theta), b = sin(theta);
-                    Mat res(srcF.size(), CV_8UC3, Scalar::all(0));
-                    double x0 = a * rho, y0 = b * rho;
-                    Point pt1, pt2;
-                    pt1.x = cvRound(x0 + 1000 * (-b));
-                    pt1.y = cvRound(y0 + 1000 * (a));
-                    pt2.x = cvRound(x0 - 1000 * (-b));
-                    pt2.y = cvRound(y0 - 1000 * (a));
-                    cv::line(res, pt1, pt2, Scalar(0, 0, 255), 1, LINE_AA);
-                    imshow("HoughLines", res);
-                    waitKey(1);
-                }
-            }
-            if ((num[left] == 0) || (num[right] == 0))
-                return;
-            rho_aver[left] /= num[left];
-            theta_aver[left] /= num[left];
-            rho_aver[right] /= num[right];
-            theta_aver[right] /= num[right];
-            // 去除错误直线并拟合左右车道线
-            _clean_lines(lines_left_right[left], num[left], rho_aver[left], theta_aver[left]);
-            _clean_lines(lines_left_right[right], num[right], rho_aver[right], theta_aver[right]);
+        // if (lines.size() > 1)
+        // {
+        //     // 遍历直线求平均，分成左右两边
+        //     for (Vec2f &line : lines)
+        //     {
+        //         double rho = line[0], theta = line[1];
+        //         // if(abs(rho-rho_NUM)>rho_Derta && abs(theta-theta_NUM)>theta_Derta || abs(theta) < 3)//最后这个是滤去一些theta错误的直线，可以不加
+        //         //     continue;
+        //         if (theta > 0)
+        //         {
+        //             rho_aver[left] += rho;
+        //             theta_aver[left] += theta;
+        //             lines_left_right[left][num[left]] = line;
+        //             num[left]++;
+        //         }
+        //         else if (theta < 0)
+        //         {
+        //             rho_aver[right] += rho;
+        //             theta_aver[right] += theta;
+        //             lines_left_right[right][num[right]] = line;
+        //             num[right]++;
+        //         }
+        //         if (param_modification_)
+        //         {
+        //             double a = cos(theta), b = sin(theta);
+        //             Mat res(srcF.size(), CV_8UC3, Scalar::all(0));
+        //             double x0 = a * rho, y0 = b * rho;
+        //             Point pt1, pt2;
+        //             pt1.x = cvRound(x0 + 1000 * (-b));
+        //             pt1.y = cvRound(y0 + 1000 * (a));
+        //             pt2.x = cvRound(x0 - 1000 * (-b));
+        //             pt2.y = cvRound(y0 - 1000 * (a));
+        //             cv::line(res, pt1, pt2, Scalar(0, 0, 255), 1, LINE_AA);
+        //             imshow("HoughLines", res);
+        //             waitKey(1);
+        //         }
+        //     }
+        //     if ((num[left] == 0) || (num[right] == 0))
+        //         return;
+        //     rho_aver[left] /= num[left];
+        //     theta_aver[left] /= num[left];
+        //     rho_aver[right] /= num[right];
+        //     theta_aver[right] /= num[right];
+        //     // 去除错误直线并拟合左右车道线
+        //     _clean_lines(lines_left_right[left], num[left], rho_aver[left], theta_aver[left]);
+        //     _clean_lines(lines_left_right[right], num[right], rho_aver[right], theta_aver[right]);
 
-            for (int i = left; i <= right; i++)
-            {
-                double a = cos(theta_aver[i]), b = sin(theta_aver[i]);
-                x[i] = ((rho_aver[i] - b * judge_line_) / a);
-                // 直线绘制
-                if (param_modification_)
-                {
-                    Mat res(srcF.size(), CV_8UC3, Scalar::all(0));
-                    double x0 = a * rho_aver[i], y0 = b * rho_aver[i];
-                    Point pt1, pt2;
-                    pt1.x = cvRound(x0 + 1000 * (-b));
-                    pt1.y = cvRound(y0 + 1000 * (a));
-                    pt2.x = cvRound(x0 - 1000 * (-b));
-                    pt2.y = cvRound(y0 - 1000 * (a));
-                    line(res, pt1, pt2, Scalar(0, 0, 255), 1, LINE_AA);
-                    line(res, Point(0, judge_line_), Point(c_end_ - c_start_, judge_line_), Scalar(255, 0, 0), 1, LINE_AA);
-                    imshow("res", res);
-                    waitKey(1);
-                }
-            }
-            double err = (x[left] + x[right]) / 2 - srcF.cols / 2;
-            // 滤除不正常的误差
-            err = (abs(err) > (c_end_ - c_start_) / 7.0) ? last_err : err;
-            if (flag = 0)
-            {
-                flag = 1;
-                last_err = err;
-            }
-            double d_err = err - last_err;
-            twist.linear.x = linear_velocity_;
-            twist.angular.z = (err != 0) ? -kp_ * err - kd_ * d_err : 0;
-            last_err = err;
-            // ROS_INFO_STREAM("err" << err << " size:" << lines.size());
-            // ROS_INFO_STREAM(minx << ' ' << maxx);
-        }
-        else
-        {
-            // 即使没有车道线的参照，仍然前进
-            twist.linear.x = linear_velocity_;
-        }
-        // ROS_INFO_STREAM(lines.size());
-        if (!param_modification_ || (param_modification_ && motor_status_))
-            cmd_vel_publisher_.publish(twist);
+        //     for (int i = left; i <= right; i++)
+        //     {
+        //         double a = cos(theta_aver[i]), b = sin(theta_aver[i]);
+        //         x[i] = ((rho_aver[i] - b * judge_line_) / a);
+        //         // 直线绘制
+        //         if (param_modification_)
+        //         {
+        //             Mat res(srcF.size(), CV_8UC3, Scalar::all(0));
+        //             double x0 = a * rho_aver[i], y0 = b * rho_aver[i];
+        //             Point pt1, pt2;
+        //             pt1.x = cvRound(x0 + 1000 * (-b));
+        //             pt1.y = cvRound(y0 + 1000 * (a));
+        //             pt2.x = cvRound(x0 - 1000 * (-b));
+        //             pt2.y = cvRound(y0 - 1000 * (a));
+        //             line(res, pt1, pt2, Scalar(0, 0, 255), 1, LINE_AA);
+        //             line(res, Point(0, judge_line_), Point(c_end_ - c_start_, judge_line_), Scalar(255, 0, 0), 1, LINE_AA);
+        //             imshow("res", res);
+        //             waitKey(1);
+        //         }
+        //     }
+        //     double err = (x[left] + x[right]) / 2 - srcF.cols / 2;
+        //     // 滤除不正常的误差
+        //     err = (abs(err) > (c_end_ - c_start_) / 7.0) ? last_err : err;
+        //     if (flag = 0)
+        //     {
+        //         flag = 1;
+        //         last_err = err;
+        //     }
+        //     double d_err = err - last_err;
+        //     twist.linear.x = linear_velocity_;
+        //     twist.angular.z = (err != 0) ? -kp_ * err - kd_ * d_err : 0;
+        //     last_err = err;
+        //     // ROS_INFO_STREAM("err" << err << " size:" << lines.size());
+        //     // ROS_INFO_STREAM(minx << ' ' << maxx);
+        // }
+        // else
+        // {
+        //     // 即使没有车道线的参照，仍然前进
+        //     twist.linear.x = linear_velocity_;
+        // }
+        // // ROS_INFO_STREAM(lines.size());
+        // if (!param_modification_ || (param_modification_ && motor_status_))
+        //     cmd_vel_publisher_.publish(twist);
     }
 
     void LineFollower::_dr_callback(lineConfig &config, uint32_t level)
