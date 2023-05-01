6978814da886ace23dc3d0dbf2e5de6f83407e9e
nano <nano@nano.com>
2023-05-01 16:16:39 +0800

    eye

--- src/my_hand_eye/arm.sh
+++ src/my_hand_eye/arm.sh
@@ -1,5 +1,5 @@
 echo 'KERNEL=="ttyUSB*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", MODE:="0777", GROUP:="dialout", SYMLINK+="ft_servo"' >/etc/udev/rules.d/ft_servo.rules
-echo 'KERNEL=="video*", ATTR{index}=="0", ATTRS{idVendor}=="0c45", ATTRS{idProduct}=="6368", MODE:="0777", GROUP:="dialout", SYMLINK+="eye"' >/etc/udev/rules.d/eye.rules
+echo 'KERNEL=="video*", ATTRS{idVendor}=="05a3", ATTRS{idProduct}=="9230", MODE:="0777", GROUP:="dialout", SYMLINK+="eye"' >/etc/udev/rules.d/eye.rules
 service udev reload
 sleep 2
 service udev restart
