e642a0b07fe7f2a6cba641d898f4dfd451236296
Fu Jiamu <fu@fu-Legion-Y9000P-IAH7H>
2023-08-22 13:32:43 +0800

    front_camera

--- src/my_hand_eye/arm.sh
+++ src/my_hand_eye/arm.sh
@@ -1,5 +1,8 @@
 echo 'KERNEL=="ttyUSB*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", MODE:="0777", GROUP:="dialout", SYMLINK+="ft_servo"' >/etc/udev/rules.d/ft_servo.rules
 echo 'KERNEL=="video*", ATTRS{idVendor}=="05a3", ATTRS{idProduct}=="9230", MODE:="0777", GROUP:="dialout", SYMLINK+="eye"' >/etc/udev/rules.d/eye.rules
+echo 'KERNEL=="video*", ATTR{index}=="0", ATTRS{idVendor}=="0601", ATTRS{idProduct}=="0405", ATTRS{serial}=="200901010001", MODE:="0777", GROUP:="dialout", SYMLINK+="front_camera"' >/etc/udev/rules.d/front_camera.rules
+echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60",ATTRS{serial}=="0003", MODE:="0777", GROUP:="dialout", SYMLINK+="fdilink_ahrs"' >/etc/udev/rules.d/fdilink_ahrs.rules
+echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", MODE:="0777", GROUP:="dialout", SYMLINK+="c_board"' >/etc/udev/rules.d/c_board.rules
 service udev reload
 sleep 2
 service udev restart
--- src/turn_on_wheeltec_robot/wheeltec_udev.sh
+++ src/turn_on_wheeltec_robot/wheeltec_udev.sh
@@ -1,7 +1,7 @@
-echo 'KERNEL=="ttyCH343USB*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="55d4",ATTRS{serial}=="0002",MODE:="0777", GROUP:="dialout", SYMLINK+="wheeltec_controller"' >/etc/udev/rules.d/wheeltec_controller.rules
-echo 'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60",ATTRS{serial}=="0001",MODE:="0777", GROUP:="dialout", SYMLINK+="wheeltec_lidar"' >/etc/udev/rules.d/wheeltec_lidar.rules
+# echo 'KERNEL=="ttyCH343USB*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="55d4",ATTRS{serial}=="0002",MODE:="0777", GROUP:="dialout", SYMLINK+="wheeltec_controller"' >/etc/udev/rules.d/wheeltec_controller.rules
+# echo 'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60",ATTRS{serial}=="0001",MODE:="0777", GROUP:="dialout", SYMLINK+="wheeltec_lidar"' >/etc/udev/rules.d/wheeltec_lidar.rules
 # echo 'KERNEL=="video*", ATTRS{idVendor}=="8086", ATTRS{idProduct}=="0b07",MODE:="0777", GROUP:="dialout", SYMLINK+="d435"' >/etc/udev/rules.d/d435.rules
-echo 'KERNEL=="video*", ATTR{index}=="0", ATTRS{idVendor}=="0c45", ATTRS{idProduct}=="6368",MODE:="0777", GROUP:="dialout", SYMLINK+="usb_cam"' >/etc/udev/rules.d/usb_cam.rules
+echo 'KERNEL=="video*", ATTR{index}=="0", ATTRS{idVendor}=="0601", ATTRS{idProduct}=="0405", ATTRS{serial}=="200901010001", MODE:="0777", GROUP:="dialout", SYMLINK+="front_camera"' >/etc/udev/rules.d/front_camera.rules
 echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60",ATTRS{serial}=="0003", MODE:="0777", GROUP:="dialout", SYMLINK+="fdilink_ahrs"' >/etc/udev/rules.d/fdilink_ahrs.rules
 echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", MODE:="0777", GROUP:="dialout", SYMLINK+="c_board"' >/etc/udev/rules.d/c_board.rules
 service udev reload
