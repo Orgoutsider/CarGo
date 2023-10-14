2178bc7b7dd627c2965a7c143b8fa4b0da38b67a
nano <nano@nano.com>
2023-10-15 02:07:08 +0800

    faster

--- src/my_hand_eye/src/my_hand_eye_start_node.cpp
+++ src/my_hand_eye/src/my_hand_eye_start_node.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     ros::init(argc, argv, "my_hand_eye_start_node");
     ros::NodeHandle nh;
     ros::NodeHandle pnh("~");
-    pnh.setParam("emulation", true);
+    // pnh.setParam("emulation", true);
     controller.init(nh, pnh);
     controller.start(true);
     return 0;
